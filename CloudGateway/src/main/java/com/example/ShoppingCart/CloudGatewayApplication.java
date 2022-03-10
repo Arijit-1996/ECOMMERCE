7bc092ef920ac0c82148099a3f73a928da2c570b
Arijit <arijitpaul36@gmail.com>
2022-03-10 19:50:19 +0530

    added

--- CloudGateway/src/main/java/com/example/ShoppingCart/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/example/ShoppingCart/CloudGatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class CloudGatewayApplication {
-
+//Adding lines
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
